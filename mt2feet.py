940bc2db44fb3842ffe1c8c0d5ccfaabd21eb97a
iamAkolab <77028341+iamAkolab@users.noreply.github.com>
2021-03-02 00:57:59 +0100

    Create mt2feet.py
    first commit....

---
+++ mt2feet.py
@@ -0,0 +1,10 @@
+from tkinter import TK, Button, Label, DoubleVar, Entry
+
+#App User Interface
+appWindow = TK()
+appWindow.title("Meter to Feet Conversion App")
+appWindow.configure(background = "grey")
+appWindow.geometry("320 x 240")
+appWindow.resizable(width = False, height = False)
+
+appWindow.mainloop()
